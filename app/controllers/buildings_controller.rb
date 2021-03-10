2323499211cec711eedfe6e1ce0c7d037a9c3260
Alejandra <marialesr.17@gmail.com>
2021-03-09 20:35:31 -0600

    modified forms and styles

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -4,8 +4,35 @@ class BuildingsController < ApplicationController
   end
 
   def new
+    @building = Building.new
+     @apartment = Apartment.pluck(:id, :name)
   end
 
   def show
+    @building = Building.find(params[:id]) #Paréntesis es opcional
+  end
+end
+
+def create
+  @building = Building.new(params[:name])
+
+  respond_to do |format|
+    if @building.save
+      format.html { redirect_to @building, notice: "Building was successfully created." }
+      format.json { render :show, status: :created, location: @building }
+    else
+      format.html { render :new, status: :unprocessable_entity }
+      format.json { render json: @building.errors, status: :unprocessable_entity }
+    end
+  end
+
+  
+  private
+  def set_building
+    @building = Building.find(params[:id])
+  end
+  #Strong params
+  def building_params
+    params.require(:building).permit(:name)
   end
 end
